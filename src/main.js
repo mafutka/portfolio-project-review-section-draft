c46fa87e5e487ddea7330291b4395b4de39f6e55
mafutka <mafutka@gmail.com>
2025-04-25 12:12:34 +0300

    start

--- src/main.js
+++ src/main.js
@@ -4,3 +4,25 @@ import Swiper from 'swiper';
 import 'swiper/css';
 
 
+async function fetchReviews() {
+    try {
+      const response = await fetch('https://portfolio-js.b.goit.study/api/reviews');
+      if (!response.ok) throw new Error('Fetch failed');
+  
+      const reviews = await response.json();
+      console.log('Reviews from backend:', reviews); // ⬅️ це покаже масив усіх відгуків
+  
+      // якщо хочеш подивитися один окремо:
+      reviews.forEach((review, index) => {
+        console.log(`Review ${index + 1}:`);
+        console.log('Author:', review.author);
+        console.log('Avatar URL:', review.avatar_url);
+        console.log('Review Text:', review.review);
+      });
+  
+    } catch (error) {
+      console.error('Error fetching reviews:', error);
+    }
+  }
+  
+  fetchReviews();
